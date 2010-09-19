3c9462ec1e79722aa10f53651c1119dca9c1b6df
ehud <ehud@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2010-09-19 08:34:10 +0000

    Changes db creation method
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/on-premise/trunk/installer@50377 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

--- steps/OperDatabaseStep.class.php
+++ steps/OperDatabaseStep.class.php
@@ -6,6 +6,10 @@ class OperDatabaseStep extends InstallStep
 
 	public function install()
 	{	
+		$sql_dir = "/app/deployment/base/sql/";
+		
+		$sql_files = parse_ini_file(myConf::get('APP_DIR').$sql_dir.'create_kaltura_db.ini', true);
+		
 		// create operational database
 		$result = DatabaseUtils::createDb(myConf::get('DB1_NAME'), myConf::get('DB1_HOST'), myConf::get('DB1_USER'), myConf::get('DB1_PASS'), myConf::get('DB1_PORT'));
 		if ($result !== true) {
@@ -13,11 +17,13 @@ class OperDatabaseStep extends InstallStep
 			return $result;
 		}
 		
-		$result = DatabaseUtils::runScript(myConf::get('APP_DIR').'/app/deployment/base/sql/create_kaltura_db.sql', myConf::get('DB1_HOST'), myConf::get('DB1_USER'), myConf::get('DB1_PASS'), myConf::get('DB1_NAME'), myConf::get('DB1_PORT'));
-		if ($result !== true) {
-			$this->addStepToError($result);
-			return $result;
-		}				
+		for ($sql_files['kaltura']['sql'] as $sql) {
+			$result = DatabaseUtils::runScript(myConf::get('APP_DIR').$sql_dir.$sql, myConf::get('DB1_HOST'), myConf::get('DB1_USER'), myConf::get('DB1_PASS'), myConf::get('DB1_NAME'), myConf::get('DB1_PORT'));
+			if ($result !== true) {
+				$this->addStepToError($result);
+				return $result;
+			}				
+		}
 		
 		// create stats database
 		$result = DatabaseUtils::createDb(myConf::get('DB_STATS_NAME'), myConf::get('DB_STATS_HOST'), myConf::get('DB_STATS_USER'), myConf::get('DB_STATS_PASS'), myConf::get('DB_STATS_PORT'));
@@ -26,10 +32,12 @@ class OperDatabaseStep extends InstallStep
 			return $result;
 		}
 		
-		$result = DatabaseUtils::runScript(myConf::get('APP_DIR').'/app/deployment/base/sql/create_stats_db.sql', myConf::get('DB_STATS_HOST'), myConf::get('DB_STATS_USER'), myConf::get('DB_STATS_PASS'), myConf::get('DB_STATS_NAME'), myConf::get('DB_STATS_PORT'));
-		if ($result !== true) {
-			$this->addStepToError($result);
-			return $result;
+		for ($sql_files['stats']['sql'] as $sql) {		
+			$result = DatabaseUtils::runScript(myConf::get('APP_DIR').$sql_dir.$sql, myConf::get('DB_STATS_HOST'), myConf::get('DB_STATS_USER'), myConf::get('DB_STATS_PASS'), myConf::get('DB_STATS_NAME'), myConf::get('DB_STATS_PORT'));
+			if ($result !== true) {
+				$this->addStepToError($result);
+				return $result;
+			}
 		}
 				
     	return true;	
