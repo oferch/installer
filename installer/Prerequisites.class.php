d213629e449a72643ca76be01beff33adfafedbb
ehud <ehud@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2010-10-05 12:23:50 +0000

    Fixed issues for sphinx to run
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/on-premise/trunk/installer@51038 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

--- installer/Prerequisites.class.php
+++ installer/Prerequisites.class.php
@@ -130,7 +130,7 @@ class Prerequisites {
     // checks that needed apache modules exist, using the given $httpd_bin
 	private function checkApacheModules($httpd_bin) {
 		// TODO: check apachectl -t to see that it is working first
-		$apache_cmd = $httpd_bin.' -M -D PHP5';
+		$apache_cmd = $httpd_bin.' -M';
 		$current_modules = OsUtils::executeReturnOutput($apache_cmd);
 				
 		foreach (Prerequisites::$apache_modules as $module) {
