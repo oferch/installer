dbcd11bf29d0e40ddd2d73b6d2699bb7f4e26174
ehud <ehud@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2010-10-17 09:27:32 +0000

    Fixed typo
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/on-premise/trunk/installer@51458 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

--- installer/Prerequisites.php
+++ installer/Prerequisites.php
@@ -1,7 +1,7 @@
 <?php
 
-include_once(realpath(dirname(__FILE__).'/DatabaseUtils.class.php');
-include_once(realpath(dirname(__FILE__).'/Log.php');
+include_once(realpath(dirname(__FILE__)).'/DatabaseUtils.class.php');
+include_once(realpath(dirname(__FILE__)).'/Log.php');
 
 define("FILE_PREREQUISITES_CONFIG", "prerequisites.ini"); // this file contains the definitions of the prerequisites that are being checked
 
