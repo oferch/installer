79fb7ddb244a8397c1a97c1fd59a4f38696f20bb
guyb <guyb@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2011-10-04 11:11:58 +0000

    qnd: make cron run under kaltura user
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/on-premise/trunk/installer@71501 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

--- install.php
+++ install.php
@@ -209,8 +209,6 @@ if ($install_output !== null) {
 if (strcasecmp($app->get('KALTURA_VERSION_TYPE'), K_TM_TYPE) === 0) {
 	$tracking_cron = sprintf("\n0 8 5 * * kaltura %s %s/admin_console/scripts/send-usage-report.php\n", $app->get('PHP_BIN'), $app->get('APP_DIR'));
 	OsUtils::appendFile($app->get('BASE_DIR').'/crontab/kaltura_crontab', $tracking_cron);
-	$cron_content = file_get_contents($app->get('BASE_DIR').'/crontab/kaltura_crontab');
-	OsUtils::appendFile('etc/crontab', $cron_content);
 }
 
 // send settings mail if possible
