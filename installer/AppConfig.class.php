c201233b8f3a6a0c4c745c3a8a49c16b71ada90c
tantan <tantan@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2013-02-19 12:24:55 +0000

    qnd: define rtmp according to red5 defaults
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/on-premise/trunk/installer@94240 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

--- installer/AppConfig.class.php
+++ installer/AppConfig.class.php
@@ -268,7 +268,7 @@ class AppConfig
 		self::$app_config[AppConfigAttribute::CORP_REDIRECT] = '';	
 		self::$app_config[AppConfigAttribute::CDN_HOST] = self::$app_config[AppConfigAttribute::KALTURA_VIRTUAL_HOST_NAME];
 		self::$app_config[AppConfigAttribute::IIS_HOST] = self::$app_config[AppConfigAttribute::KALTURA_VIRTUAL_HOST_NAME];
-		self::$app_config[AppConfigAttribute::RTMP_URL] = self::extractHostName(self::$app_config[AppConfigAttribute::KALTURA_VIRTUAL_HOST_NAME]);
+		self::$app_config[AppConfigAttribute::RTMP_URL] = 'rtmp://' . AppConfig::get(AppConfigAttribute::KALTURA_VIRTUAL_HOST_NAME) . '/oflaDemo';
 		self::$app_config[AppConfigAttribute::BASE_HOST_NO_PORT] = self::extractHostName(self::$app_config[AppConfigAttribute::KALTURA_VIRTUAL_HOST_NAME]);
 		self::$app_config[AppConfigAttribute::MEMCACHE_HOST] = self::extractHostName(self::$app_config[AppConfigAttribute::KALTURA_VIRTUAL_HOST_NAME]);
 		self::$app_config[AppConfigAttribute::GLOBAL_MEMCACHE_HOST] = self::extractHostName(self::$app_config[AppConfigAttribute::KALTURA_VIRTUAL_HOST_NAME]);
