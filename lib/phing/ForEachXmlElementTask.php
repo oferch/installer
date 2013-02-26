9f1857c33bcf4dfeaa8f34a632164238b8bcc78f
tantan <tantan@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2013-02-26 13:47:22 +0000

    qnd: run both child nodes and source path nodes
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/on-premise/trunk/installer@94520 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

--- lib/phing/ForEachXmlElementTask.php
+++ lib/phing/ForEachXmlElementTask.php
@@ -8,55 +8,55 @@ class ForEachXmlElementTask extends Task
 	 * @var string
 	 */
 	private $elementPrefix;
-	
+
 	/**
 	 * Name of parameter to pass to callee for XML element xPath
 	 * @var string
 	 */
 	private $xPathParam;
-	
+
 	/**
 	 * Indicates that root node name should be included in the xPath
 	 * @var bool
 	 */
 	private $xPathSkipRoot;
-	
+
 	/**
 	 * Sets the start point in the XML
 	 * @var string
 	 */
 	private $xPathStart = null;
-	
+
 	/**
 	 * Indicates that root node should be skipped
 	 * @var bool
 	 */
 	private $skipRoot;
-	
+
 	/**
 	 * Indicates that child nodes should be processed
 	 * @var bool
 	 */
 	private $recursive = true;
-	
+
 	/**
 	 * The path to the XML file.
 	 * @var string
 	 */
 	private $file = null;
-	
+
 	/**
 	 * PhingCallTask that will be invoked w/ calleeTarget.
 	 * @var PhingCallTask
 	 */
 	private $callee;
-	
+
 	/**
 	 * Target to execute.
 	 * @var string
 	 */
 	private $calleeTarget;
-	
+
 	/**
 	 * The init method: Do init steps.
 	 */
@@ -68,7 +68,7 @@ class ForEachXmlElementTask extends Task
 		$this->callee->setLocation($this->getLocation());
 		$this->callee->init();
 	}
-	
+
 	/**
 	 * The main entry point method.
 	 */
@@ -83,14 +83,14 @@ class ForEachXmlElementTask extends Task
 		if (is_null($this->calleeTarget)) {
 			throw new BuildException("You must supply a target to perform");
 		}
-		
+
 		$callee = $this->callee;
 		$callee->setTarget($this->calleeTarget);
 		$callee->setInheritAll(true);
 		$callee->setInheritRefs(true);
-		
+
 		$xml = new SimpleXMLElement(file_get_contents($this->file));
-		
+
 		if($this->xPathStart)
 		{
 			$this->log("Loading element by xPath [$this->xPathStart]", Project::MSG_INFO);
@@ -116,48 +116,48 @@ class ForEachXmlElementTask extends Task
 			$this->eachElement($xml);
 		}
 	}
-	
+
 	public function foreachElement(SimpleXMLElement $xml, $xPath = '')
 	{
 		foreach($xml as $nodeName => $node)
 			$this->eachElement($node, "$xPath/$nodeName");
 	}
-	
+
 	public function eachElement(SimpleXMLElement $xml, $xPath = '')
 	{
 		$nodeId = uniqid();
 		if(isset($xml['id']))
 			$nodeId = $xml['id'];
 		$nodeId = $this->project->replaceProperties($nodeId);
-			
+
 		$nodeName = $xml->getName();
 		$elementChildrenCount = $xml->count();
-		
+
 		$prop = $this->callee->createProperty();
 		$prop->setOverride(true);
 		$prop->setName("$this->elementPrefix.id");
 		$prop->setValue($nodeId);
 		$this->log("Setting param '$this->elementPrefix.id' to value '$nodeId'", Project::MSG_VERBOSE);
-		
+
 		$prop = $this->callee->createProperty();
 		$prop->setOverride(true);
 		$prop->setName("$this->elementPrefix.name");
 		$prop->setValue($nodeName);
 		$this->log("Setting param '$this->elementPrefix.name' to value '$nodeName'", Project::MSG_VERBOSE);
-		
+
 		$prop = $this->callee->createProperty();
 		$prop->setOverride(true);
 		$prop->setName("$this->elementPrefix.count");
 		$prop->setValue($elementChildrenCount);
 		$this->log("Setting param '$this->elementPrefix.count' to value '$elementChildrenCount'", Project::MSG_VERBOSE);
-		
+
 		$elementContent = "$xml";
 		$prop = $this->callee->createProperty();
 		$prop->setOverride(true);
 		$prop->setName("$this->elementPrefix.content");
 		$prop->setValue($elementContent);
 		$this->log("Setting param '$this->elementPrefix.content' to value '$elementContent'", Project::MSG_VERBOSE);
-	
+
 		foreach($xml->attributes() as $attributeName => $attributeValue)
 		{
 			$attributeValue = $this->project->replaceProperties($attributeValue);
@@ -168,7 +168,7 @@ class ForEachXmlElementTask extends Task
 			$prop->setValue("$attributeValue");
 			$this->log("Setting param '$elementAttributesParam' to value '$attributeValue'", Project::MSG_VERBOSE);
 		}
-		
+
 		if (!is_null($this->xPathParam))
 		{
 			$prop = $this->callee->createProperty();
@@ -177,27 +177,28 @@ class ForEachXmlElementTask extends Task
 			$prop->setValue($xPath);
 			$this->log("Setting param '$this->xPathParam' to value '$xPath'", Project::MSG_VERBOSE);
 		}
-		
+
 		$this->callee->main();
-		
+
 		if(!$this->recursive)
 			return;
-			
+
+		if(isset($xml['sourcePath']))
+		{
+			$sourcePath = strval($xml['sourcePath']);
+			if (!file_exists($sourcePath))
+				throw new BuildException("Supplied file path [$sourcePath] doesn't exist.");
+
+			$sourceXml = new SimpleXMLElement(file_get_contents($sourcePath));
+			$this->foreachElement($sourceXml, $xPath);
+		}
+
 		if($elementChildrenCount)
 		{
 			$this->foreachElement($xml, $xPath);
 		}
-		elseif(isset($xml['sourcePath']))
-		{
-			$sourcePath = strval($xml['sourcePath']);
-			if (!file_exists($sourcePath)) 
-				throw new BuildException("Supplied file path [$sourcePath] doesn't exist.");
-			
-			$sourceXml = new SimpleXMLElement(file_get_contents($sourcePath));
-			$this->foreachElement($sourceXml, $xPath);
-		}
 	}
-	
+
 	/**
 	 * @param string $elementPrefix
 	 */
