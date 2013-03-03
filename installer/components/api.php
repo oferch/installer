befb31fea5aeeebae118b2bbeffcdb74e7bc3922
tantan <tantan@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2013-03-03 06:49:44 +0000

    qnd: add listen directive only if the port is not the deafault.
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/on-premise/trunk/installer@94804 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

--- installer/components/api.php
+++ installer/components/api.php
@@ -1,5 +1,16 @@
 <?php
 
+if(AppConfig::get(AppConfigAttribute::KALTURA_VIRTUAL_HOST_PORT) != 80)
+{
+	$configFiles = glob(AppConfig::get(AppConfigAttribute::APP_DIR) . '/configurations/apache/kaltura*.conf');
+	$configLine = 'Listen ' . AppConfig::get(AppConfigAttribute::KALTURA_VIRTUAL_HOST_PORT);
+	foreach($configFiles as $configFile)
+	{
+		$content = $configLine . "\n" . file_get_contents($configFile);
+		file_put_contents($configFile, $content);
+	}
+}
+
 $this->restartApache();
 
 return true;
