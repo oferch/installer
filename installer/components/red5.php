046fa610e4c82ba5d98612d34d950befe02b98dd
tantan <tantan@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2013-03-07 11:11:27 +0000

    qnd: rtmp url is already correct in the ui confs
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/on-premise/trunk/installer@95094 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

--- installer/components/red5.php
+++ installer/components/red5.php
@@ -7,9 +7,4 @@ $red5Addition = array ('class' => 'kLocalPathUrlManager');
 $urlManagersValues[AppConfig::get(AppConfigAttribute::ENVIRONMENT_NAME)] = $red5Addition;
 OsUtils::writeToIniFile($location, $urlManagersValues);
 
-//Retrieve KCW uiconf ids
-$uiconfIds = $this->extractKCWUiconfIds();
-Logger::logMessage(Logger::LEVEL_USER, "If you are insterested in recording entries from webcam, please adjust the RTMP server URL in each of the following uiConfs:\r\n". implode("\r\n", $uiconfIds));
-Logger::logMessage(Logger::LEVEL_USER, "By replacing 'rtmp://yoursite.com/oflaDemo' with 'rtmp://". AppConfig::get(AppConfigAttribute::ENVIRONMENT_NAME) . "/oflaDemo");
-
 return true;
