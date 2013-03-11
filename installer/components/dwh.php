1e30b72551c98d6fbf0f59bfeb45003f0931db7d
tantan <tantan@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2013-03-11 12:23:23 +0000

    qnd: logs only
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/on-premise/trunk/installer@95288 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

--- installer/components/dwh.php
+++ installer/components/dwh.php
@@ -6,7 +6,7 @@ if((!AppConfig::get(AppConfigAttribute::DB1_CREATE_NEW_DB)) && (DatabaseUtils::d
 }
 else
 {
-	Logger::logMessage(Logger::LEVEL_USER, "Creating data warehouse");
+	Logger::logMessage(Logger::LEVEL_INFO, "Creating data warehouse");
 	if (!OsUtils::execute(sprintf("%s/setup/dwh_setup.sh -h %s -P %s -u %s -p %s -d %s ", AppConfig::get(AppConfigAttribute::DWH_DIR), AppConfig::get(AppConfigAttribute::DB1_HOST), AppConfig::get(AppConfigAttribute::DB1_PORT), AppConfig::get(AppConfigAttribute::DB_ROOT_USER), AppConfig::get(AppConfigAttribute::DB_ROOT_PASS), AppConfig::get(AppConfigAttribute::DWH_DIR)))) {
 		return "Failed running data warehouse initialization script";
 	}
