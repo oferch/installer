ccd0b6b4e975aeb86e5f17f120002fc5803a65fb
tantan <tantan@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2013-03-13 16:24:42 +0000

    qnd: different protocols should be written to different config files
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/on-premise/trunk/installer@95526 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

--- installer/components/api.php
+++ installer/components/api.php
@@ -1,8 +1,19 @@
 <?php
 
-if(AppConfig::get(AppConfigAttribute::KALTURA_VIRTUAL_HOST_PORT) != 80 && AppConfig::get(AppConfigAttribute::KALTURA_VIRTUAL_HOST_PORT) != 443)
+if(AppConfig::get(AppConfigAttribute::ENVIRONMENT_PROTOCOL) == 'http' && AppConfig::get(AppConfigAttribute::KALTURA_VIRTUAL_HOST_PORT) != 80)
 {
 	$configFile = AppConfig::get(AppConfigAttribute::APP_DIR) . '/configurations/apache/kaltura.conf';
+	if(file_exists($configFile))
+	{
+		$configLine = 'Listen ' . AppConfig::get(AppConfigAttribute::KALTURA_VIRTUAL_HOST_PORT);
+		$content = $configLine . "\n" . file_get_contents($configFile);
+		file_put_contents($configFile, $content);
+	}
+}
+
+if(AppConfig::get(AppConfigAttribute::ENVIRONMENT_PROTOCOL) == 'https' && AppConfig::get(AppConfigAttribute::KALTURA_VIRTUAL_HOST_PORT) != 443)
+{
+	$configFile = AppConfig::get(AppConfigAttribute::APP_DIR) . '/configurations/apache/kaltura.ssl.conf';
 	if(file_exists($configFile))
 	{
 		$configLine = 'Listen ' . AppConfig::get(AppConfigAttribute::KALTURA_VIRTUAL_HOST_PORT);
