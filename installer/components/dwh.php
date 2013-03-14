15b6b5df78fe61fc2be3cfa558bdd63c9fc89428
tantan <tantan@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2013-03-14 08:27:41 +0000

    qnd: logs only
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/on-premise/trunk/installer@95548 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

--- installer/components/dwh.php
+++ installer/components/dwh.php
@@ -1,10 +1,6 @@
 <?php
 
-if(!AppConfig::get(AppConfigAttribute::DB1_CREATE_NEW_DB) && (DatabaseUtils::dbExists(AppConfig::get(AppConfigAttribute::DWH_HOST), AppConfig::get(AppConfigAttribute::DWH_PORT), AppConfig::get(AppConfigAttribute::DWH_DATABASE_NAME)) === true))
-{
-	Logger::logMessage(Logger::LEVEL_USER, sprintf("Skipping '%s' database creation", AppConfig::get(AppConfigAttribute::DWH_DATABASE_NAME)));
-}
-else
+if(AppConfig::get(AppConfigAttribute::DB1_CREATE_NEW_DB) || !DatabaseUtils::dbExists(AppConfig::get(AppConfigAttribute::DWH_HOST), AppConfig::get(AppConfigAttribute::DWH_PORT), AppConfig::get(AppConfigAttribute::DWH_DATABASE_NAME)))
 {
 	Logger::logMessage(Logger::LEVEL_INFO, "Creating data warehouse");
 	if (!OsUtils::execute(sprintf("%s/setup/dwh_setup.sh -h %s -P %s -u %s -p %s -d %s ", AppConfig::get(AppConfigAttribute::DWH_DIR), AppConfig::get(AppConfigAttribute::DB1_HOST), AppConfig::get(AppConfigAttribute::DB1_PORT), AppConfig::get(AppConfigAttribute::DB_ROOT_USER), AppConfig::get(AppConfigAttribute::DB_ROOT_PASS), AppConfig::get(AppConfigAttribute::DWH_DIR)))) {
