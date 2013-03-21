1c7c3724619451a34dc8f7d9515f06a1d8990570
orlyl <orlyl@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2013-03-21 10:38:50 +0000

    def: 7652 - support DWH (db) installation when password is empty
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/on-premise/trunk/installer@95950 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

--- installer/components/dwh.php
+++ installer/components/dwh.php
@@ -3,7 +3,12 @@
 if(AppConfig::get(AppConfigAttribute::DB1_CREATE_NEW_DB) || !DatabaseUtils::dbExists(AppConfig::get(AppConfigAttribute::DWH_HOST), AppConfig::get(AppConfigAttribute::DWH_PORT), AppConfig::get(AppConfigAttribute::DWH_DATABASE_NAME)))
 {
 	Logger::logMessage(Logger::LEVEL_INFO, "Creating data warehouse");
-	if (!OsUtils::execute(sprintf("%s/setup/dwh_setup.sh -h %s -P %s -u %s -p %s -d %s ", AppConfig::get(AppConfigAttribute::DWH_DIR), AppConfig::get(AppConfigAttribute::DB1_HOST), AppConfig::get(AppConfigAttribute::DB1_PORT), AppConfig::get(AppConfigAttribute::DB_ROOT_USER), AppConfig::get(AppConfigAttribute::DB_ROOT_PASS), AppConfig::get(AppConfigAttribute::DWH_DIR)))) {
+	if (!AppConfig::get(AppConfigAttribute::DB_ROOT_PASS)) {
+		$cmd = sprintf("%s/setup/dwh_setup.sh -h %s -P %s -u %s -d %s ", AppConfig::get(AppConfigAttribute::DWH_DIR), AppConfig::get(AppConfigAttribute::DB1_HOST), AppConfig::get(AppConfigAttribute::DB1_PORT), AppConfig::get(AppConfigAttribute::DB_ROOT_USER), AppConfig::get(AppConfigAttribute::DWH_DIR));
+	} else {
+		$cmd = sprintf("%s/setup/dwh_setup.sh -h %s -P %s -u %s -p %s -d %s ", AppConfig::get(AppConfigAttribute::DWH_DIR), AppConfig::get(AppConfigAttribute::DB1_HOST), AppConfig::get(AppConfigAttribute::DB1_PORT), AppConfig::get(AppConfigAttribute::DB_ROOT_USER), AppConfig::get(AppConfigAttribute::DB_ROOT_PASS), AppConfig::get(AppConfigAttribute::DWH_DIR));
+	}
+	if (!OsUtils::execute($cmd)){
 		return "Failed running data warehouse initialization script";
 	}
 }
