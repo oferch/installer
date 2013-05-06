33a317ddcce7a5107d967a579b6a1ba419c87270
tantan <tantan@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2013-05-06 13:31:20 +0000

    qnd: always install dwh, it's not only db creation
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/on-premise/trunk/installer@97826 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

--- installer/components/dwh.php
+++ installer/components/dwh.php
@@ -1,16 +1,13 @@
 <?php
 
-if(AppConfig::get(AppConfigAttribute::DB1_CREATE_NEW_DB) || !DatabaseUtils::dbExists(AppConfig::get(AppConfigAttribute::DWH_HOST), AppConfig::get(AppConfigAttribute::DWH_PORT), AppConfig::get(AppConfigAttribute::DWH_DATABASE_NAME)))
-{
-	Logger::logMessage(Logger::LEVEL_INFO, "Creating data warehouse");
-	if (!AppConfig::get(AppConfigAttribute::DB_ROOT_PASS)) {
-		$cmd = sprintf("%s/setup/dwh_setup.sh -h %s -P %s -u %s -d %s ", AppConfig::get(AppConfigAttribute::DWH_DIR), AppConfig::get(AppConfigAttribute::DB1_HOST), AppConfig::get(AppConfigAttribute::DB1_PORT), AppConfig::get(AppConfigAttribute::DB_ROOT_USER), AppConfig::get(AppConfigAttribute::DWH_DIR));
-	} else {
-		$cmd = sprintf("%s/setup/dwh_setup.sh -h %s -P %s -u %s -p %s -d %s ", AppConfig::get(AppConfigAttribute::DWH_DIR), AppConfig::get(AppConfigAttribute::DB1_HOST), AppConfig::get(AppConfigAttribute::DB1_PORT), AppConfig::get(AppConfigAttribute::DB_ROOT_USER), AppConfig::get(AppConfigAttribute::DB_ROOT_PASS), AppConfig::get(AppConfigAttribute::DWH_DIR));
-	}
-	if (!OsUtils::execute($cmd)){
-		return "Failed running data warehouse initialization script";
-	}
+Logger::logMessage(Logger::LEVEL_INFO, "Creating data warehouse");
+if (!AppConfig::get(AppConfigAttribute::DB_ROOT_PASS)) {
+	$cmd = sprintf("%s/setup/dwh_setup.sh -h %s -P %s -u %s -d %s ", AppConfig::get(AppConfigAttribute::DWH_DIR), AppConfig::get(AppConfigAttribute::DB1_HOST), AppConfig::get(AppConfigAttribute::DB1_PORT), AppConfig::get(AppConfigAttribute::DB_ROOT_USER), AppConfig::get(AppConfigAttribute::DWH_DIR));
+} else {
+	$cmd = sprintf("%s/setup/dwh_setup.sh -h %s -P %s -u %s -p %s -d %s ", AppConfig::get(AppConfigAttribute::DWH_DIR), AppConfig::get(AppConfigAttribute::DB1_HOST), AppConfig::get(AppConfigAttribute::DB1_PORT), AppConfig::get(AppConfigAttribute::DB_ROOT_USER), AppConfig::get(AppConfigAttribute::DB_ROOT_PASS), AppConfig::get(AppConfigAttribute::DWH_DIR));
+}
+if (!OsUtils::execute($cmd)){
+	return "Failed running data warehouse initialization script";
 }
 
 return true;
