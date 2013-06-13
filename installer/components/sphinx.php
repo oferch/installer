a4c6eea605221aaf4759f50d388c82e6590f0d92
tantan <tantan@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2013-06-13 15:21:00 +0000

    qnd: upgrade sphinx
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/on-premise/trunk/installer@99742 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

--- installer/components/sphinx.php
+++ installer/components/sphinx.php
@@ -6,11 +6,12 @@ if(AppConfig::get(AppConfigAttribute::UPGRADE_FROM_VERSION))
 	$appDir = AppConfig::get(AppConfigAttribute::APP_DIR);
 	$populateScripts = array(
 		"$appDir/deployment/base/scripts/populateSphinxCategories.php",
+		"$appDir/deployment/base/scripts/populateSphinxEntries.php",
+		"$appDir/deployment/base/scripts/populateSphinxKusers.php",
+		"$appDir/deployment/base/scripts/populateSphinxCaptionAssetItem.php",
 		"$appDir/deployment/base/scripts/populateSphinxCategoryKusers.php",
 		"$appDir/deployment/base/scripts/populateSphinxCuePoints.php",
-		"$appDir/deployment/base/scripts/populateSphinxEntries.php",
 		"$appDir/deployment/base/scripts/populateSphinxEntryDistributions.php",
-		"$appDir/deployment/base/scripts/populateSphinxKusers.php",
 		"$appDir/deployment/base/scripts/populateSphinxTags.php",
 	);
 	
