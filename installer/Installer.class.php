05b33d0cab2de02e939b496bfba0d0a3c5011450
tantan <tantan@6b8eccd3-e8c5-4e7d-8186-e12b5326b719>
2013-06-14 06:31:36 +0000

    qnd: install dwh after setting the permissions
    git-svn-id: svn+ssh://kelev.kaltura.com/usr/local/kalsource/on-premise/trunk/installer@99758 6b8eccd3-e8c5-4e7d-8186-e12b5326b719

--- installer/Installer.class.php
+++ installer/Installer.class.php
@@ -393,6 +393,9 @@ class Installer
 		if(!$this->changeDirsAndFilesPermissions())
 			return "Failed to set files permissions";
 
+		if(!$this->installDWH())
+			return "Failed to install DWH";
+
 		if(OsUtils::isLinux())
 		{
 			if(!$this->restartApache(true))
@@ -891,8 +894,40 @@ class Installer
 		return true;
 	}
 
+	private function installDWH ()
+	{
+		if(!in_array('dwh', $this->components))
+			return true;
+			
+		$arguments = sprintf('-h %s -P %s -u %s -d %s', AppConfig::get(AppConfigAttribute::DB1_HOST), AppConfig::get(AppConfigAttribute::DB1_PORT), AppConfig::get(AppConfigAttribute::DB_ROOT_USER), AppConfig::get(AppConfigAttribute::DWH_DIR));
+		if (AppConfig::get(AppConfigAttribute::DB_ROOT_PASS))
+			$arguments .= ' -p ' . AppConfig::get(AppConfigAttribute::DB_ROOT_PASS);
+			
+		if(AppConfig::get(AppConfigAttribute::DB1_CREATE_NEW_DB))
+		{
+			Logger::logMessage(Logger::LEVEL_INFO, "Creating data warehouse");
+			$cmd = sprintf("%s/setup/dwh_setup.sh $arguments", AppConfig::get(AppConfigAttribute::DWH_DIR));	
+			if (!OsUtils::execute($cmd)){
+				return "Failed running data warehouse initialization script";
+			}
+		}
+		elseif(AppConfig::get(AppConfigAttribute::UPGRADE_FROM_VERSION))
+		{
+			Logger::logMessage(Logger::LEVEL_INFO, "Upgrading data warehouse");
+			$cmd = sprintf("%s/ddl/migrations/20130606_falcon_to_gemini/Falcon2Gemini.sh $arguments", AppConfig::get(AppConfigAttribute::DWH_DIR));
+			if (!OsUtils::execute($cmd)){
+				return "Failed running data warehouse upgrade script";
+			}
+		}
+		
+		return true;
+	}
+
 	private function populateSphinx ()
 	{
+		if(!in_array('sphinx', $this->components))
+			return true;
+			
 		if(AppConfig::get(AppConfigAttribute::UPGRADE_FROM_VERSION))
 		{
 			Logger::logMessage(Logger::LEVEL_INFO, "Populating old content to the sphinx");
